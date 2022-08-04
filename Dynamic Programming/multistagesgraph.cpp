b503746a3ee9d09e599107d2bfe9c9ceaafe3ce8
dylanphe <dylanphe@g.ucla.edu>
2022-08-03 22:05:26 -0700

    MSGshortestpath fin

--- Dynamic Programming/multistagesgraph.cpp
+++ Dynamic Programming/multistagesgraph.cpp
@@ -51,6 +51,7 @@ void multistageGraph::print()
     }
 }
 
+//O(V^2)
 void multistageGraph::MSGshortestpath()
 {
     //Create adjacency matrix for DP 
@@ -70,16 +71,55 @@ void multistageGraph::MSGshortestpath()
         }
     }
     
+    //cost to store the min cost from v to sink
+    //path to store the shortest path take from any vertex w to sink
+    //shortestPath to store the shortest path taken in each stage
     int cost[V], path[V], shortestPath[stage]; 
     //Last node has no path. therefore its cost is also 0
     cost[V-1] = 0;
-    path[V-1] = 0;
+    path[V-2] = INT_MAX;
+    //shortestPath in stage 1 is the src vertex and in stage n is the sink vertex
+    shortestPath[0] = 0;
+    shortestPath[stage-1] = V-1;
+    //Update cost and path
     for (int i = V - 2; i >= 0; i--)
     {
         int min = INT_MAX;
+        for (int j = i + 1; j < V; j++)
+        {
+            if (w[i][j] != 0 && w[i][j] + cost[j] < min)
+            {
+                //suboptimal formula
+                min = w[i][j] + cost[j];
+                path[i] = j;
+            }
+        }
+        cost[i] = min;
     }
 
-    /*for (int i = 0; i < V; i++)
+    for(int i = 1; i < stage-1; i++)
+    {
+        shortestPath[i] = path[shortestPath[i-1]];
+    }
+
+    cout << "Shortest Path = ";
+    for (int i = 0; i < stage; i++)
+    {
+        cout << shortestPath[i] << " ";
+    }
+    
+    cout << "\nMinimum Cost = " << cost[0];
+
+    
+
+    /*cout << "\n";
+
+    for (int i = 0; i < V; i++)
+    {
+        cout << path[i] << " ";
+    }
+
+    for (int i = 0; i < V; i++)
     {
         for (int j = 0; j < V; j++)
         {
